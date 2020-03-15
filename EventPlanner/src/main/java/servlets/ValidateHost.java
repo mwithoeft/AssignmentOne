cbc74248f934176627e223a56fe556cf393987fc
Moritz Withöft <moritz_pit.withoeftQfh-bielefeld.de>
2020-03-15 21:46:51 +1300

    Create Host

--- EventPlanner/src/main/java/servlets/ValidateHost.java
+++ EventPlanner/src/main/java/servlets/ValidateHost.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package servlets;
 
-import beans.EventBean;
 import beans.HostBean;
 import java.io.IOException;
 import javax.inject.Inject;
@@ -15,7 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import tables.CustomEvent;
 import tables.CustomHost;
 import tables.CustomHostDB;
 
